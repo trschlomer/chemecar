06364c7189486e57b709c6038ba6e2a45948cdeb
JBrooklyn <59520243+JBrooklyn@users.noreply.github.com>
2020-07-09 15:13:19 -0400

    Update urls.py

--- chemecardb/urls.py
+++ chemecardb/urls.py
@@ -12,9 +12,28 @@ urlpatterns = [
     url(r'^updatemems/', views.updatemems, name="updatemems"),
 
     url(r'^scheduling/$', views.scheduling, name="schedule"),
+    url(r'^insertsched/', views.insertched, name="insertsched"),
+    url(r'^deletesched/', views.deletesched, name="deletesched"),
+    url(r'^updatesched/', views.updatesched, name="updatesched"),
+    
     url(r'^material/$', views.material, name="materials"),
+    url(r'^insertmems/', views.insertmems, name="insertmems"),
+    url(r'^deletemems/', views.deletemems, name="deletemems"),
+    url(r'^updatemems/', views.updatemems, name="updatemems"),
+   
     url(r'^car/$', views.car, name="car"),
+    
     url(r'^powmech/$', views.powmech, name="power mechanism"),
+    url(r'^insertpowmech/', views.insertpowmech, name="insertpowmech"),
+    url(r'^deletepowmech/', views.deletepowmech, name="deletepowmech"),
+    
+    
     url(r'^trial/$', views.trial, name="trials"),
+    url(r'^inserttrial/', views.inserttrial, name="inserttrial"),
+    url(r'^deletetrial/', views.deletetrial, name="deletetrial"),
+    
     url(r'^stopmech/$', views.stopmech, name="stop mechanism"),
+    url(r'^insertstopmech/', views.insertstopmech, name="insertstopmech"),
+    url(r'^deletestopmech/', views.deletestopmech, name="deletestopmech"),
+    
 ]
